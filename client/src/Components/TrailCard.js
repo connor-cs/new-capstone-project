8c6118dd290c9faa7942104e1ba4939b1c0ed30d
Connor <connor.cs@protonmail.com>
2022-10-04 18:39:21 -0400

    adding google maps

--- client/src/Components/TrailCard.js
+++ client/src/Components/TrailCard.js
@@ -13,8 +13,9 @@ export default function TrailCard({trail}) {
     
     <div key={trail.id} className={"trailcard"}>
       <h2>{trail.name}</h2>
-      <p>{trail.length}miles</p>
+      <p>Length: {trail.length} miles</p>
       <p>{trail.description}</p>
+      <p>Location: {trail.directions}</p>
       <p>{trail.city} {trail.state}</p>
       <button onClick={handleClick}>♥</button>
     </div>
