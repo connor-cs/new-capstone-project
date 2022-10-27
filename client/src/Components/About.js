0233a2127a1f9dda2da4603dea33df12e677a20e
Connor <connor.cs@protonmail.com>
2022-10-26 20:49:30 -0400

    more styling

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -4,10 +4,10 @@ export default function About() {
   return (
     <main className='about-page'>
       <div className='about-div'>
+        <div className='about-text'>
         <h3 className='black-text'>About</h3>
-        
         <p>This app allows you to search for hiking trails near you by searching your city and state.
-          The results include the name, a brief description, the length and the location.</p>
+          The results include the name, a brief description, the length, and the location plotted on Google Maps.</p>
           <br></br>
         <h4 className='black-text'><ul>The technologies used include:</ul></h4>
         <li>A front-end built with React with a Google Maps integration</li>
@@ -20,6 +20,7 @@ export default function About() {
         <li>Signup for an account and login/logout</li>
         <li>Search for trails by city and state</li>
         <li>Add a trail to their list of favorites</li>
+        </div>
       </div>
     </main>
   )
--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -3,47 +3,60 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { LoginContext } from "./LoggedInContext";
+import Button from '@mui/material/Button';
 
 export default function Signup() {
-
-  const [errors, setErrors] = useState([])
-  const [password, setPassword] = useState("")
-  const [username, setUsername] = useState("")
-  const { setLoggedIn, user, setUser } = useContext(LoginContext)
-  const navigate = useNavigate()
+  const [errors, setErrors] = useState([]);
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const { setLoggedIn, user, setUser } = useContext(LoginContext);
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     fetch("/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password })
-    })
-      .then(res => {
-        if (res.ok) {
-          res.json()
-            .then(data => setUser(data))
-          setLoggedIn(true)
-          navigate('/')
-        }
-        else {
-          res.json()
-            .then(data => setErrors([...data.errors]))
-            .then(console.log(errors))
-        }
-      })
+      body: JSON.stringify({ username, password }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => setUser(data));
+        setLoggedIn(true);
+        navigate("/");
+      } else {
+        res
+          .json()
+          .then((data) => setErrors([...data.errors]))
+          .then(console.log(errors));
+      }
+    });
   }
-
 
   return (
     <main className="signup-page">
-      <form onSubmit={handleSubmit}>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}></input>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button type="submit">Signup</button>
-      </form>
+      <div className="signup-form">
+        <form onSubmit={handleSubmit} >
+          <label>Username: </label>
+          <input
+          className="text-input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          <br></br>
+          <br></br>
+          <label>Password: </label>
+          <input
+          className="text-input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <br></br>
+          <br></br>
+          <Button variant="outlined" type="submit">Signup</Button>
+        </form>
+      </div>
       {errors ? errors : null}
     </main>
   );
